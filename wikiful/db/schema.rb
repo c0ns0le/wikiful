7459a2b2671522cbb0093e432c0fc7aec8d3af6b
Daniel Bogart <daniel.e.bogart@gmail.com>
2014-07-27 16:18:13 -0700

    Added DB

--- wikiful/db/schema.rb
+++ wikiful/db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140726174530) do
+ActiveRecord::Schema.define(version: 20131120162928) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "article_categories", force: true do |t|
     t.integer  "article_id"
@@ -23,6 +26,20 @@ ActiveRecord::Schema.define(version: 20140726174530) do
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
