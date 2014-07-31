2ef2904dde08e37a7514b8c98068859d7c10067d
Daniel Bogart <daniel.e.bogart@gmail.com>
2014-07-31 13:49:19 -0700

    Additional style to article show view

--- wikiful/app/controllers/articles_controller.rb
+++ wikiful/app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
 
 	def show
 		@article = Article.find(params[:id])
+		@category = Category.find(Article.find(params[:id]).category_ids.first)
 	end
 
 	def new
