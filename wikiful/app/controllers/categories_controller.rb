9045ca76edc73f513485d3bd26e400915faafc05
Daniel Bogart <daniel.e.bogart@gmail.com>
2014-07-31 09:39:49 -0700

    Added index and show for categories

--- wikiful/app/controllers/categories_controller.rb
+++ wikiful/app/controllers/categories_controller.rb
@@ -1,9 +1,15 @@
-class ArticlesController < ApplicationController
+class CategoriesController < ApplicationController
 	def index
 		@categories = Category.order('name').limit(25)
 	end
 
 	def show
-		@categories = Category.find(params[:id])
+		@category = Category.find(params[:id])
+		@article = Article.find(params[:id])
+	end
+
+	private
+	def category_params
+		params.require(:category).permit(:name, :category_ids => [])
 	end
 end
--- wikiful/app/models/category.rb
+++ wikiful/app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
 	has_many :articles, through: :article_categories
+	has_many :article_categories #needed to be declared on it's own line
 	validates :name, presence: true
 end
